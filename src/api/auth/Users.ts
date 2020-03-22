c57fe3dc293f1c0c7e95ed3b377a086534aa2d51
Kacper Kopański <kacperkopanski12@outlook.com>
2020-03-22 11:40:37 +0100

    update endpoint api/auth/sing-in

---
+++ src/api/auth/Users.ts
@@ -0,0 +1,12 @@
+export const Users = [
+  {
+    userName: 'kowalski15',
+    password: '12345@',
+    fullName: 'Janusz Kowalski',
+  },
+  {
+    userName: 'nowak34',
+    password: '12345@',
+    fullName: 'Bartek Nowak',
+  },
+];

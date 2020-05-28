18ad36cdc472b4a8d94f31e9110a3748b52691f0
Kacper Kopański <kacperkopanski12@outlook.com>
2020-05-28 22:29:06 +0200

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { config} from 'dotenv';
+config();
+const port: number = Number(process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3650);
+  await app.listen( port | 3650);
 }
 bootstrap();
